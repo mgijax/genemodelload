3187efdfcfe3411809e8510616c1840075c32b1e
Lori Corbani <lori.corbani@jax.org>
2024-10-04 14:21:14 -0400

    wts2-1543/e4g-17/VISTA load 2024-08

--- bin/MGIreg.gff3.py
+++ bin/MGIreg.gff3.py
@@ -1,55 +1,34 @@
 
 '''
-# Report:
-#       Tab-delimited file of Ensembl, NCBI, VISTA regulatory region coordinates
+#Report:
+#       Tab-delimited file of Ensembl and VISTA regulatory region coordinates
 #       (Cre and More Project (CREAM))
 #
-#   1. chromosome
-#   2. source of feature: mrk_location_cache.provider
-#   3. gene feature: if NCBI, use GFF/field 3, else use SO Term Name
+#   1. chromosome: e.g. '19' 
+#   2. source of feature: mrk_location_cache.provider.  'Ensembl Reg' or 'VISTA'
+#   3. gene feature: MGI Feature Type e.g. CTCF binding site or enhancer 
 #   4. start coordinate
 #   5. end coordinate
 #   6. score: '.'
 #   7. strand: '.'
 #   8. frame: '.'
 #   9. feature attributes: Creator; Sequence_tag_ID; GenBank_ID; DBxref; Type
-#	  where: 
+#	- where: 
 #         'ID' an internal ID
 #   	  'Name' Marker Symbol
 #	  'description' Marker name
 #         'curie' Marker MGI ID
-#	  'Dbxref'  ENSEMBL, NCBI, VISTA IDs
-#	  'bound_start ENSEMBL, NCBI from the ensembl/ncbi regulatory gff file
-#     'bound_end ENSEMBL, NCBI from the ensembl/ncbi regulatory gff file
-#     VISTA: no bounds
-#     'mgi_type MGI feature type
-#     'so_term_name SO term
+#	  'Dbxref'  ENSEMBL or VISTA IDs
+#	  'bound_start ENSEMBL only from the ensembl.gff
+#         'bound_end ENSEMBL only from the ensembl regulatory gff file
+#             VISTA: no bounds
+#         'mgi_type MGI feature type
+#         'so_term_name SO term
 #
 # Usage:
 #     ${PYTHON} MGIreg.gff3.py 
 #
-# 1. Search MGI for all Mouse Marker with Ensembl (222) and VISTA (223) 
-#       where marker status = 'official'
-# 2. Search MGI for all Mouse Marker with NCBS (59), 
-#       where marker status = 'official' 
-#       and provider = 'NCBI' 
-#       and marker type = 'Other Genome Feature'
-# 3. Search Ensembl GFF and save Seq ID , field 3 (gff term), bound_start, bound_end where:
-#       where line contains 'ID', 'bound_start', 'bound_end'
-# 4. Search NCBI GFF and save Seq ID, field 3 (gff term), field 4 (bound_end), field 5 (bound_start) where:
-#       where line contains 'RefSeqFE' and 'Dbxref=GeneID:'
-# 5. For each Marker in (1&2): 
-#   a. if VISTA, then use VISTA Marker info from MGI (1)
-#   b. if Ensembl then use Ensembl Marker info from MGI (1) and GFF (3)
-#   c. if NCBI, then use NCBI Marker info from MGI (3) and GFF (4)
-# 6. For VISTA, there is 1 MGI row per Marker
-# 7. For Ensembl, there is 1 GFF row per Marker
-# 8. For NCBI, there can be >= 1 GFF row per Marker
-#
 # History:
-#
-# 08/29/2024    lec
-#   wts2-1538/fl2-951/Add NCBI regulatory regions/biological_region to MGIreg.gff3 file
 #
 # 5/18/2022 sc - created
 #
@@ -57,6 +36,7 @@
  
 import sys
 import os
+import string
 import mgi_utils
 import reportlib
 import db
@@ -87,26 +67,7 @@ ensemblGFFUrl = os.getenv('ENSEMBL_GFF_URL')
 # creation time of sourceEnsemblGFF
 ensemblGFFTimeStamp = time.ctime(os.path.getmtime(sourceEnsemblGFF))
 
-# end ENSEMBL GFF Configuration
-
-# NCBI GFF Configuration
-
-# zipped version of NCBI gff - filename from source
-sourceNcbiGFF = os.getenv('NCBI_GFF')
-
-# local unzipped sourceNcbiGFF file from which to parse bounds
-ncbiGFF = os.getenv('NCBI_GFF_DEFAULT')
-
-# just the file name w/o extension
-baseNcbiGFF = os.path.splitext(os.path.basename(sourceNcbiGFF))[0]
-
-# URL from which we get sourceNCBIGFF
-ncbiGFFUrl = os.getenv('NCBI_GFF_URL')
-
-# creation time of sourceNCBIGFF
-ncbiGFFTimeStamp = time.ctime(os.path.getmtime(sourceNcbiGFF))
-
-# end NCBI GFF Configuration
+# VISTA GFF Configuration
 
 # where to write the gff file and its name
 outputDir = os.getenv('OUTPUTDIR')
@@ -115,9 +76,9 @@ fileName = 'MGIreg.gff3'
 # mapping of MCV ID to SO ID and Term
 mcvIdToSODict = {}
 
-# mapping of Ensembl ID, NCBI ID to 1 or more bound_start and bound_end 
-# from ensembl regulatory, ncbi gff file
-boundDict = {}
+# mapping of Ensembl ID to bound_start and bound_end from ensembl regulatory 
+# gff file
+boundsDict = {}
 
 def init():
     global fp, finalResults
@@ -126,31 +87,45 @@ def init():
     fp = open('%s/%s' % (outputDir, fileName), 'w')
 
     #
-    # create MCV & SO ID lookup
+    # create SO ID to SO Term lookup
     #
-    results = db.sql('''
-        select a1.accid as mcvID, a2.accid as soID, 
-            t1._term_key as mcvTermKey, t1.term as mcvTerm, 
-            t3.term as soTerm, t3._term_key as sotermKey
-        from acc_accession a1, voc_term t1, acc_accession a2, acc_accession a3, voc_term t3
-        where a1._logicaldb_key = 146
-        and a1._mgitype_key = 13
-        and a1._object_key = t1._term_key
-        and t1._term_key = a2._object_key
-        and a2._mgitype_key = 13
-        and a2._logicaldb_key = 145
-        and a2.accid = a3.accid
-        and a3._mgitype_key = 13
-        and a3._logicaldb_key = 145
-        and a3._object_key = t3._term_key
-        and t3._vocab_key = 138
-        ''', 'auto')
-    for r in results:
-        mcvIdToSODict[r['mcvID']] = [r['soID'], r['soTerm']]
 
-    db.sql('''
-        (
-        select a1.accid as seqID, a1._logicaldb_key, a2.accid as markerID,
+    # get MCV terms with their accids
+    db.sql('''select a.accid as mcvID, t._term_key as mcvTermKey, 
+        t.term as mcvTerm, t.note as mcvNote
+        into temporary table mcv
+        from voc_term t, acc_accession a
+        where a._logicaldb_key = 146
+        and a._mgitype_key = 13
+        and a._object_key = t._term_key''', None)
+
+    # pull in the SO ID - it has same _term_key, but different ldb
+    db.sql('''select a.accid as soID, m.*
+        into temporary table mcv_so
+        from acc_accession a, mcv m
+        where m.mcvTermKey = a._object_key
+        and a._mgitype_key = 13 
+        and a._logicaldb_key = 145''', None)
+
+    # now get the SO term for the SO ID = this requires joining to the accession table AND to the term table for the SO vocab
+    # we are not using everything in the select clause, but I left it be for cut/paste debugging if needed later - this query
+    # was a bear to put together.
+    results = db.sql('''select t.term as soTerm, t.note as soNote, ms.soID, 
+        t._term_key as 
+            soTermKey,  ms.mcvTerm, ms.mcvNote, ms.mcvID, ms.mcvTermKey
+        from voc_term t, acc_accession a, mcv_so ms
+        where ms.soID = a.accid
+        and a._mgitype_key = 13
+        and a._logicaldb_key = 145
+        and a._object_key = t._term_key
+        and t._vocab_key = 138''', 'auto')
+
+    # not sure if we need the SO ID but getting it now just in case
+    for r in results:
+        soList = [r['soID'], r['soTerm']]
+        mcvIdToSODict[r['mcvID']] = soList
+
+    db.sql('''select a1.accid as seqID, a1._logicaldb_key, a2.accid as markerID,
             c.provider, c.genomicchromosome, c. startcoordinate, 
             c.endcoordinate, c.strand, m.symbol, m.name, t.name as markerType, 
             mcv.term as mcvTerm
@@ -163,58 +138,30 @@ def init():
         and a1._object_key = a2._object_key
         and a2._mgitype_key = 2
         and a2._logicaldb_key = 1
-        and a2.preferred = 1
         and c._marker_key = m._marker_key
-        and m._marker_status_key = 1
         and m._marker_type_key = t._marker_type_key
         and c._marker_key = mcv._marker_key
-        and mcv.qualifier = 'D' 
-        union
-        select a1.accid as seqID, a1._logicaldb_key, a2.accid as markerID,
-            c.provider, c.genomicchromosome, c. startcoordinate, 
-            c.endcoordinate, c.strand, m.symbol, m.name, t.name as markerType, 
-            mcv.term as mcvTerm
-        from mrk_location_cache c, acc_accession a1, acc_accession a2, 
-            mrk_marker m, mrk_mcv_cache mcv, mrk_types t
-        where a1._logicaldb_key in (59)
-        and a1._mgitype_key = 2
-        and a1._object_key = c._marker_key
-        and c.provider = 'NCBI'
-        and a1._object_key = a2._object_key
-        and a2._mgitype_key = 2
-        and a2._logicaldb_key = 1
-        and a2.preferred = 1
-        and c._marker_key = m._marker_key
-        and m._marker_status_key = 1
-        and m._marker_type_key = 9
-        and m._marker_type_key = t._marker_type_key
-        and c._marker_key = mcv._marker_key
-        and mcv.qualifier = 'D' 
-        )
-        ''', None)
+        and mcv.qualifier = 'D' ''', None)
 
     db.sql('''create index idx1 on ev(mcvTerm)''', None)
 
-    finalResults = db.sql('''
-        select ev.*, a.accid as mcvID 
+    finalResults = db.sql('''select ev.*, a.accid as mcvID 
         from ev, voc_term t, acc_accession a
         where ev.mcvTerm = t.term
         and t._term_key = a._object_key
         and a._mgitype_key = 13
         and a._logicaldb_key = 146
-        order by ev.genomicchromosome, ev.startcoordinate 
-        ''', 'auto')
+        order by ev.genomicchromosome, ev.startcoordinate ''', 'auto')
 
     # Create lookup of bounds by ensembl id
+
     fpE = open(ensemblGFF, 'r', encoding='latin-1')
     for line in fpE:
+        col9 = str.split(line, TAB)[8]
+        fields = str.split(col9, ';')
         ens_id = '' 
         bound_start = ''
         bound_end = ''
-        tokens = str.split(line, TAB)
-        gffTerm = tokens[2]
-        col9 = tokens[8]
-        fields = str.split(col9, ';')
         for f in fields:
             if f.find('ID') == 0:
                 # Example: ID=TF_binding_site:ENSMUSR00000612461
@@ -227,37 +174,7 @@ def init():
         if ens_id == '' or bound_start == '' or bound_end == '':
             print('Missing: ens_id: %s bound_start: %s bound_end: %s' % (ens_id, bound_start, bound_end))
         else:
-            if ens_id not in boundDict:
-                boundDict[ens_id] = []
-            boundDict[ens_id].append([bound_start, bound_end, gffTerm])
-
-    # Create lookup of bounds by ncbi id
-    fpN = open(ncbiGFF, 'r', encoding='latin-1')
-    for line in fpN:
-        if line.startswith('#'):
-            continue
-        if line.find('RefSeqFE') < 0:
-            continue
-        if line.find('Dbxref=GeneID:') < 0:
-            continue
-        ncbi_id = ''
-        bound_start = ''
-        bound_end = ''
-        tokens = str.split(line, TAB)
-        gffTerm = tokens[2]
-        bound_start = tokens[4]
-        bound_end = tokens[3]
-        t1 = str.split(line, 'Dbxref=GeneID:')
-        t2 = t1[1].split(';')
-        ncbi_id = t2[0]
-
-        if ncbi_id == '' or bound_start == '' or bound_end == '':
-            print('Missing: ncbi_id: %s bound_start: %s bound_end: %s' % (ncbi_id, bound_start, bound_end))
-        else:
-            if ncbi_id not in boundDict:
-                boundDict[ncbi_id] = []
-            boundDict[ncbi_id].append([bound_start, bound_end, gffTerm])
-
+            boundsDict[ens_id] = [bound_start, bound_end]
     return 0
 
 def writeHeader():
@@ -272,7 +189,7 @@ def writeHeader():
 # Genome build: %s
 #
 # The MGIreg gff3 file is generated by combining information from multiple sources.
-# Regulatory features and genome coordinates are obtained from VISTA, the Ensembl Regulatory Build & NCBI Regulatory Build.
+# Regulatory features and genome coordinates are obtained from VISTA and the Ensembl Regulatory Build.
 # MGI transforms coordinates to genome build GRCm39 where necessary.
 # Nomenclature, identifiers, and cross references come from MGI. Provider representations of regulatory
 # features are preserved in MGI, with no attempt to identify regulatory feature equivalence between providers.
@@ -288,18 +205,11 @@ def writeHeader():
 #
 # ----------------------------------
 #
-# ncbi regulatory build
-# File: %s
-# File url: %s
-# File date used: %s
-#
-# ----------------------------------
-#
 # VISTA
 # File: VISTA.fasta
 # File url: https://enhancer.lbl.gov/cgi-bin/imagedb3.pl?search.result=yes;form=search;action=search;page_size=20000;show=1;search.form=no;search.org=Mouse;search.sequence=1
 # File date used: 2022-03-24 
-#\n''' % (date, mAssembly, baseEnsemblGFF, ensemblGFFUrl, ensemblGFFTimeStamp, baseNcbiGFF, ncbiGFFUrl, ncbiGFFTimeStamp))
+#\n''' % (date, mAssembly, baseEnsemblGFF, ensemblGFFUrl, ensemblGFFTimeStamp))
 
 def writeGffFile():
 
@@ -314,14 +224,11 @@ def writeGffFile():
     column3 = ''     # SO term
     column4 = ''     # startCoordinate
     column5 = ''     # endCoordinate
-    column9 = ''     # extra information
+    column9 = ''
 
     # template for column nine
     column9EnsTemplate = 'ID=%s;Name=%s;description=%s;curie=%s;Dbxref=%s;bound_start=%s;bound_end=%s;mgi_type=%s;so_term_name=%s'
-    column9NcbiTemplate1 = 'ID=%s;Name=%s;description=%s;curie=%s;Dbxref=%s;bound_start=%s;bound_end=%s;mgi_type=%s;so_term_name=%s'
-    column9NcbiTemplate2 = 'ID=%s;Name=%s;description=%s;curie=%s;Dbxref=%s;bound_start=%s;bound_end=%s'
     column9VisTemplate = 'ID=%s;Name=%s;description=%s;curie=%s;Dbxref=%s;mgi_type=%s;so_term_name=%s'
-
     # components of column 9
     iid = 'reg%s'           # some generated internal id
     name = ''               # marker symbol
@@ -340,7 +247,8 @@ def writeGffFile():
     
     print ('Rows in the input: %s' % len(finalResults))
     for r in finalResults:
-
+        bound_start = ''
+        bound_end = ''
         generatedId += 1
         displayId = str(generatedId).zfill(6)
         seqID = r['seqID']
@@ -355,67 +263,34 @@ def writeGffFile():
         provider = r['provider']
         if r['_logicaldb_key'] == 222:
             provider = 'ENSEMBL'
-        elif r['_logicaldb_key'] == 59:
-            provider = 'NCBI'
         dbxref = '%s:%s' % ( provider, seqID)
-
         # get start and end as int
         startcoordinate = str(int(r['startcoordinate']))
         endCoordinate = str(int(r['endcoordinate']))
+        if seqID in boundsDict:
+            bound_start = boundsDict[seqID][0]
+            bound_end = boundsDict[seqID][1]
 
         column1 = r['genomicchromosome']
         column2 = provider
+        column3 = soTermName
         column4 = startcoordinate
         column5 = endCoordinate
-
-        # only 1 GFF row per Marker
-        if provider == 'VISTA':
-
-            column3 = soTermName
+        if provider == 'ENSEMBL':
+            column9 = column9EnsTemplate % (iid % displayId, r['symbol'], r['name'], r['markerID'], dbxref, bound_start, bound_end, r['mcvTerm'], soTermName)
+        else: # VISTA
             column9 = column9VisTemplate % (iid % displayId, r['symbol'], r['name'], r['markerID'], dbxref, r['mcvTerm'], soTermName)
-            fp.write(column1 + TAB)
-            fp.write(column2 + TAB)
-            fp.write(column3 + TAB)
-            fp.write(column4 + TAB)
-            fp.write(column5 + TAB)
-            fp.write(column6 + TAB)
-            fp.write(column7 + TAB)
-            fp.write(column8 + TAB)
-            fp.write(column9 + CRT)
-            continue
-
-        # > 1 GFF row per Marker
-        if seqID in boundDict:
-
-            #print MGI level row here
-
-            for b in boundDict[seqID]:
-                bound_start = b[0]
-                bound_end = b[1]
-                gffTerm = b[2]
-
-                if provider == 'ENSEMBL':
-                    column3 = soTermName
-                    column9 = column9EnsTemplate % (iid % displayId, r['symbol'], r['name'], r['markerID'], dbxref, bound_start, bound_end, r['mcvTerm'], soTermName)
-                elif provider == 'NCBI':
-                    column3 = gffTerm
-                    if column3 == 'biological_region':
-                        column9 = column9NcbiTemplate1 % (iid % displayId, r['symbol'], r['name'], r['markerID'], dbxref, bound_start, bound_end, r['mcvTerm'], soTermName)
-                    else:
-                        column9 = column9NcbiTemplate2 % (iid % displayId, r['symbol'], r['name'], r['markerID'], dbxref, bound_start, bound_end)
-
-                fp.write(column1 + TAB)
-                fp.write(column2 + TAB)
-                fp.write(column3 + TAB)
-                fp.write(column4 + TAB)
-                fp.write(column5 + TAB)
-                fp.write(column6 + TAB)
-                fp.write(column7 + TAB)
-                fp.write(column8 + TAB)
-                fp.write(column9 + CRT)
+        fp.write(column1 + TAB)
+        fp.write(column2 + TAB)
+        fp.write(column3 + TAB)
+        fp.write(column4 + TAB)
+        fp.write(column5 + TAB)
+        fp.write(column6 + TAB)
+        fp.write(column7 + TAB)
+        fp.write(column8 + TAB)
+        fp.write(column9 + CRT)
 
     return 0
-
 #
 # Main
 #
